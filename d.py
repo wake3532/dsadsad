0275862d5a8671b9d01b9b2d39592c7d05241818
wake3532 <70120646+wake3532@users.noreply.github.com>
2020-09-06 18:17:24 +0900

    Update d.py

--- d.py
+++ d.py
@@ -1,31 +1,43 @@
+# To make a discord app you need to download discord.py with pip
 #-- coding: utf-8 --
 import discord
 import asyncio
-import os
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix='wa')
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'{bot.user.id}')
+    print(f'{client.user.id}')
 
-@bot.event
+@client.event
 async def on_guild_join(guild):
-    guild1 = bot.get_guild(742180737910046830)
-    servername = guild.name
-    category = discord.utils.get(guild1.categories, id=(752061799217758219))
-    text = await guild1.create_text_channel(name=f"【:gear:】ㅣ{servername}", category=category)
-    invite = await guild.create_invite(reason="Wasekay 서버등록에 의해 자동으로 생성된 초대링크입니다.")
-    await text.send(invite)
-    
-@bot.command()
-async def 홍보방법(ctx):
-# message 쓰고싶으면 message = ctx.message하고 하시면 됨
+    category=await guild.create_category(name="[홍보서버]")
+    text = await category.create_text_channel(name="【:gear:】ㅣwasekay")
+    embed = discord.Embed(title="WaseKay", description="다양한 장르의 서버를 찾고, 다른 유저와 놀아보세요! \n ※ 해당 채널에서 잡담은 자제해주세요. \n 서포트 서버에서 서버 신청을 하실 수 있습니다. [서포트 서버](https://discord.gg/QJyQZkM)",
+    colour = discord.Colour.gold()
+    )
+    await text.send(embed=embed)
+
+@client.event
+async def on_message(message):
+    if message.content == 'wa홍보방법':
         embed = discord.Embed(title="Wasekay 홍보방법!", description="Wasekay 홍보는 굉장히 쉬워요!\n\n먼저 봇을 초대해보세요!\nㄴ 서버가 인식되면 Wasekay 홍보가 자동으로 시작해요.\nㄴ채널 이동은 상관없지만, 홍보 메시지는 절대로 지우지 마세요!\n\n채널이 생기면 자동으로 홍보를 해요!\nㄴ만약 누군가 당신의 홍보 채널에서 괴롭힌다구요? 바로 관리자를 불러보세요! 바로 달려올거에요!\n\n[봇 초대하기](https://discord.com/oauth2/authorize?client_id=746384600112038014&permissions=8&scope=bot)",
         colour = discord.Colour.purple()
     )
-        await ctx.send(embed=embed)
+        await message.channel.send(embed=embed)
+
+    if message.content == '&서버신청':
+        servername = message.guild.name
+        serverid = message.guild.id
+        print(f'{servername} - {serverid}')
+
+    if message.content == '&서버등록':
+        guild = client.get_guild(742180737910046830)
+        servername = message.guild.name
+        category = discord.utils.get(message.guild.categories, id=(752061799217758219))
+        text = await guild.create_text_channel(name=f"【:gear:】ㅣ{servername}", category=category)
+        invite = await message.channel.create_invite(reason="Wasekay 서버등록에 의해 자동으로 생성된 초대링크입니다.")
+        await text.send(invite)
 
 access_token = os.environ["BOT_TOKEN"]
 bot.run(access_token)
